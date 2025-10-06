7e28785e75e3c024cb1f8c37065735e171fa24ce
Simranjeet <simranjeet96mail@gmail.com>
2025-10-06 23:54:51 +0530

    Combining client and server in same repo

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+
+// connect to MongoDB
+mongoose.connect("mongodb+srv://simranjeets:13npozpoxm9dUe34@cluster0.kashzhe.mongodb.net/MDB?retryWrites=true&w=majority&appName=Cluster0")
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((error) => console.error(`Error connecting to MongoDB: ${error}`));
+
+// listen to PORT
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
